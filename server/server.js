77157def3e52e6f437962c08bae3e0d4275c3995
luzalisson245 <luzalisson245@gmail.com>
2021-11-13 19:56:31 +0000

    definindo os endpoints

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const TaskRoutes = require('./routes/Task');
 
 app.use(bodyParser.json());
+app.use('/api/tasks', TaskRoutes);
 
 const port = process.env.PORT || 3001;
 
