9ce468f33cd5be180a610f86b9942472ef2f4da2
luzalisson245 <luzalisson245@gmail.com>
2021-11-13 21:22:22 +0000

    corrigindo numero da porta para conexao

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(cors());
 
 app.use('/api/tasks', TaskRoutes);
 
-const port = process.env.PORT || 27017;
+const port = process.env.PORT || 3001;
 
 app.listen(port, function () {
     console.log('Server listening on port ' + port);
