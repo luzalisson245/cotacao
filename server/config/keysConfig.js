196ae71f71467a5cd34640a5ba7b2316076a1fd0
luzalisson245 <luzalisson245@gmail.com>
2021-11-13 20:45:43 +0000

    criando banco de dados

---
+++ server/config/keysConfig.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongodbUri: 'mongodb+srv://luzalisson245:wlmbuthn@cluster0.ew4ps.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"'
+  };
--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require('./config/keysConfig.js');
+const TaskRoutes = require('./routes/Tasks.js');
+const cors = require('cors');
+
 const app = express();
-const TaskRoutes = require('./routes/Task');
+
+mongoose.connect(config.mongodbUri, { useNewUrlParser: true })
+    .then(() => console.log('Database connected'))
+    .catch((err) => console.log('Error on connection', err));
+
+//mongoose.set('useFindAndModify', -1);
 
 app.use(bodyParser.json());
+app.use(cors());
+
 app.use('/api/tasks', TaskRoutes);
 
 const port = process.env.PORT || 3001;
 
-app.listen(port, function() {
-  console.log('Server listening on port ' + port);
+app.listen(port, function () {
+    console.log('Server listening on port ' + port);
 });
